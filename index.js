6c76189515c44c2a9a5431345a1141bbdd52a5a5
PC 2 <jesus.alvarez.pas@gmail.com>
2026-06-07 13:31:51 -0400

    refactor: ordenar consolelog y variable de promesa

--- index.js
+++ index.js
@@ -6,29 +6,25 @@ const productos = [
 ]
 
 const productosEnStock = productos.filter(producto => producto.stock > 0);
+console.log('Los productos en stock son:',productosEnStock);
 
 productosEnStock.map(producto => producto.precio *= 0.85);
+console.log('Productos con el descuento agregado:',productosEnStock);
 
 const totalInventario = productosEnStock.reduce((acumulador,valor)=>acumulador+(valor.precio*valor.stock),0);
+console.log('El valor total del inventario es:',totalInventario);
 
-const productoCaro = productosEnStock.find(producto => producto.precio >10);
+const productoCaro = productosEnStock.find(producto => producto.precio >=1000);
+console.log('El producto mas caro es:',productoCaro);
 
 async function obtenerDatos() {
   try {
-    const respuesta = await productoCaro;
-    // const datos = await respuesta.json();
-    console.log('Los datos obtenidos de la respuesta del fetch son:',respuesta);
+    const respuesta = await productosEnStock;
+    console.log('Los datos obtenidos de la respuesta del fetch son: ',respuesta);
   } catch (error) {
-    console.error("Algo falló:", error);
+    console.error("Sucedio un el siguiente error al intentar obtener datos:", error);
   }
 }
 
 obtenerDatos();
-
-
-
-// console.log(productos);
-// console.log(productosEnStock);
-// console.log(totalInventario);
-// console.log(productoCaro);
 
